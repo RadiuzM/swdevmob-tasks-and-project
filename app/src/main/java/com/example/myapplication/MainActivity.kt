90d10315e3f43bb390030de3007e2866d5089ca2
RadiuzM <andre.olkinuora@gmail.com>
2025-04-15 14:42:56 +0300

    core-elements task files

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,33 +1,30 @@
 package com.example.myapplication
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.widget.Button
-import android.widget.EditText
-import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-import androidx.activity.enableEdgeToEdge
 
 class MainActivity : AppCompatActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val firstNumEditText: EditText = findViewById(R.id.firstNumEditText)
-        val secondNumEditText: EditText = findViewById(R.id.secondNumEditText)
-        val addButton: Button = findViewById(R.id.AddBtn)
-        val resultTextView: TextView = findViewById(R.id.resultTextView)
 
-        addButton.setOnClickListener {
-            val firstNum = firstNumEditText.text.toString().toIntOrNull()
-            val secondNum = secondNumEditText.text.toString().toIntOrNull()
-            if (firstNum != null && secondNum != null) {
-                val sum = firstNum + secondNum //test
-                resultTextView.text = "Result: $sum"
-            } else {
-                resultTextView.text = "Please enter valid numbers!"
-            }
+        val secondActivityBtn: Button = findViewById(R.id.secondActivityBtn)
+        secondActivityBtn.setOnClickListener {
+            val intent = Intent(this, SecondActivity::class.java)
+            intent.putExtra("message.SOMETHING", "HELLO YOU")
+            startActivity(intent)
+        }
+
+        val googleBtn: Button = findViewById(R.id.googleBtn)
+        googleBtn.setOnClickListener {
+            val google = "https://www.google.com"
+            val webAddress = Uri.parse(google)
+            val goToGoogle = Intent(Intent.ACTION_VIEW, webAddress)
+
+            startActivity(goToGoogle)
         }
     }
 }
